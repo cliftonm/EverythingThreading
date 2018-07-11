066a9b42456c1cfb19fd065619a01b56baa45e55
Marc Clifton <marc.clifton@gmail.com>
2018-07-11 13:24:15 -0400

    WIP.

---
+++ CooperativeThreading/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+// A demo of cooperative threading in C#, aka fibers.
+
+namespace CooperativeThreading
+{
+    public class CooperativeManager
+    {
+        protected Queue<Action> fibers = new Queue<Action>();
+
+        public void Continue(Action next)
+        {
+        }
+    }
+
+    public class CoopTask1
+    {
+        public void DoWork(CooperativeManager cm)
+        {
+            Thread.Sleep(1000);
+            cm.Continue(() => Thread.Sleep(1000));
+            cm.Continue(() => Thread.Sleep(1000));
+            cm.Continue(() => Thread.Sleep(1000));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
